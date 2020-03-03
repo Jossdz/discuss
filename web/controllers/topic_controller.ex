b93ed60d00cd333fa63f3ec479c81c96e097314c
Joss <carloscorrea.dev@gmail.com>
2020-03-03 17:45:58 -0600

    redirect and topic creation

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -13,9 +13,17 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} -> 
+        conn
+        |> put_flash(:info, "Topic created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} -> 
         render conn, "new.html", changeset: changeset
     end
+  end
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -16,12 +16,14 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", TopicController, :index
 
     # verbo - ruta - controller - función
     get "/topics/new", TopicController, :new
 
     post "/topics", TopicController, :create
+    
+    get "/topics/:id/edit", TopicController, :edit
   end
 
   # Other scopes may use custom stacks.
