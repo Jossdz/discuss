13ffa761c9f68ecb8c07f3c07ca6ee1c7dd384bd
Joss <carloscorrea.dev@gmail.com>
2020-03-02 18:06:08 -0600

    topic form and post route

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,21 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
+  end
+end
